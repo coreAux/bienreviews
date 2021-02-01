3795c76f5cce5d68a6d41d9287e66c905bc8c424
Mickey <57460035+coreAux@users.noreply.github.com>
2021-02-01 11:40:04 +0100

    Delete carrierwave.rb

--- config/initializers/carrierwave.rb
+++
@@ -1,9 +0,0 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider:              'AWS',
-    aws_access_key_id:     'AKIAT64OYNNNML6VD2Z2',
-    aws_secret_access_key: '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',
-    region:                'eu-north-1',
-  }
-  config.fog_directory  = 'superhi-mickey-bien-reviews'
-end
